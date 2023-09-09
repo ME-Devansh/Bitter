ebda7db1ade26b09ab1e9ba75ddfb5a45f8491a7
devanshdp <devansh.pant@eka.care>
2023-09-10 00:04:37 +0530

    Shift to windows

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
-import { type AppType } from "next/app";
-import { api } from "~/utils/api";
-import "~/styles/globals.css";
+// page/_app.tsx
+import { ClerkProvider } from "@clerk/nextjs";
+import type { AppProps } from "next/app";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+}
 
-
-export default api.withTRPC(MyApp);
+export default MyApp;
